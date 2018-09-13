80fac8dcf7b32078a3c12e3a66aa2d26fc1f488a
Traci <traci.kamp@blackburn.edu>
2018-09-12 20:47:46 -0500

    Use a link element where the anchor is

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
@@ -194,7 +195,8 @@ class BooksApp extends React.Component {
               </div>
             </div>
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              {/* <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a> */}
+              <Link to="/search">Add a book</Link>
             </div>
           </div>
         )}
