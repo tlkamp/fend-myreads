3d6904c29b3957af67be50a541d31254f0550bfe
Traci <traci.kamp@blackburn.edu>
2018-09-15 17:53:50 -0500

    Remove unused import

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Book from './Book';
 import * as BooksApi from './BooksAPI';
 import BooksGrid from './BooksGrid';
 
